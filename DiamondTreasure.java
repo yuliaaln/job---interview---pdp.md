240a07384f1441df08de0ad1920baf1ee5c1022d
yuliaaln <144923542+yuliaaln@users.noreply.github.com>
2023-12-29 19:17:11 +0700

    DiamondTreasure.java

--- DiamondTreasure.java
+++ DiamondTreasure.java
@@ -27,7 +27,7 @@ public class DiamondTreasure {
         {'#', ' ', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#', '#'},
         {'#', ' ', '#', '#', ' ', '#', '#', 'C', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#', '#'},
         {'#', ' ', '#', '#', ' ', '#', '#', '#', ' ', '#', '#', '#', '#', ' ', '#', ' ', '#', '#'},
-        {'#', 'C', '#', '#', ' ', '#', '#', '#', ' ', '#', 'C', ' ', ' ', ' ', '#', ' ', '#', '#'},
+        {'#', 'C', '#', '#', ' ', '#', '#', '#', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', '#', '#'},
         {'#', '#', '#', '#', ' ', '#', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', ' ', '#', '#'},
         {'#', '#', ' ', ' ', ' ', '#', ' ', '£', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#'},
         {'#', '#', ' ', '#', '#', '#', ' ', ' ', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#'},
@@ -42,10 +42,10 @@ public class DiamondTreasure {
     private static int playerX;
     private static int playerY;
     private static int treasures = 0; // Jumlah harta karun yang telah dikumpulkan
-    private static int totalTreasures = 6; // Total harta karun yang harus dikumpulkan
+    private static int totalTreasures = 5; // Total harta karun yang harus dikumpulkan
     private static int energy = 250;
 
-    //method mindahin player
+    // method untuk memindahkan pemain
     private static void movePlayer(int moveX, int moveY) {
         int newX = playerX + moveX;
         int newY = playerY + moveY;
@@ -53,25 +53,28 @@ public class DiamondTreasure {
         if (isValidMove(newX, newY)) {
             char newPosition = maze[newX][newY];
 
-            //Karkter mengambil koin dan energi bertambah
+            // Karakter mengambil koin dan energi bertambah
             if (newPosition == 'C') {
                 maze[newX][newY] = ' ';
                 energy += 8;
                 System.out.println("Anda mendapatkan koin! Energi +8");
-            //// Karakter mengambil dan mengumpulkan harta karun
-            } else if (newPosition == 'T') {
-                if (treasures == totalTreasures - 1) {
-                    System.out.println("Anda telah mengumpulkan semua harta karun. Selamat!");
-                    System.exit(0);
-                } else {
-                    System.out.println("Anda belum mengumpulkan semua harta karun.");
+            }
+            // Karakter mengambil dan mengumpulkan harta karun
+            else if (newPosition == 'T') {
+                treasures++;
+                System.out.println("Anda telah mengumpulkan harta karun sebanyak : " + treasures);
+
+                if (treasures == totalTreasures) {
+                    System.out.println("Selamat kamu menang dan berhasil keluar!");
+                    System.exit(0); // Pemain keluar dan memenangkan permainan
                 }
-            //Energi berkurang jika karakter menginjak ular
-            } else if (newPosition == '£') {
+            }
+            // Energi berkurang jika karakter menginjak ular
+            else if (newPosition == '£') {
                 energy -= 5;
                 System.out.println("Anda terkena ular! Energi -5");
             }
-            //Hapus dan update posisi karakter
+            // Hapus dan update posisi karakter
             maze[playerX][playerY] = ' ';
             playerX = newX;
             playerY = newY;
@@ -87,7 +90,7 @@ public class DiamondTreasure {
         }
     }
 
-    //Agar karakter tidak bisa melewati batas labirin
+    // Agar karakter tidak bisa melewati batas labirin
     private static boolean isValidMove(int x, int y) {
         return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length && maze[x][y] != '#';
     }
@@ -111,20 +114,21 @@ public class DiamondTreasure {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    
+
         // Inisialisasi posisi karakter manusia dan labirin lainnya
         setPlayerPosition(1, 1); // Contoh: karakter manusia ditempatkan di koordinat (1, 1)
-    
+
         // Memulai permainan dan loop utama
         boolean program = true;
         char input;
         do {
             printMaze();
-    
+
             System.out.println("Energi: " + energy);
+            System.out.println("Total treasure : " + treasures);
             System.out.print("Masukkan perintah (w/a/s/d untuk gerak, q untuk keluar): ");
             input = scanner.next().charAt(0);
-    
+
             switch (input) {
                 case 'w':
                     movePlayer(-1, 0); // Bergerak ke atas
@@ -142,17 +146,17 @@ public class DiamondTreasure {
                     if (treasures == totalTreasures) {
                         System.out.println("Keluar permainan.");
                         program = false;
-                    }else{
-                        System.out.println("Harta karun (T) Belum terkumpul semua, anda tidak bisa keluar permainan!");
+                    } else {
+                        System.out.println("Harta karun (T) belum terkumpul, anda tidak bisa keluar permainan!");
                     }
                     break;
                 default:
                     System.out.println("Perintah tidak valid!");
                     break;
             }
-    
+
         } while (program);
-    
+
         scanner.close();
     }
 }
